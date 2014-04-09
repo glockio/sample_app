75acaf86ccb2e9509b0fa47fd2e62cf52af3e775
Greg Laughlin <greg.laughlin81@hotmail.com>
2014-04-08 17:45:58 -0700

    add crud, refactor to partials

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,9 +1,11 @@
 class ReadingsController < ApplicationController
-
- 	before_action :reading_params, only:[:create]
+  before_action :get_reading, only: [:edit, :update, :destroy]
+ 	before_action :reading_params, only:[:create, :update]
   
   def new
   	@reading=Reading.new
+    @recent_readings=Reading.recent_readings
+    @button="Submit"
   end
 
   def create 
@@ -12,17 +14,43 @@ class ReadingsController < ApplicationController
   		redirect_to root_path
   		flash[:success]="Your Reading was saved!"
   	else
-  		render 'new'
+      @recent_readings=Reading.recent_readings # QUESTION! Is there a way to use render 'new' and not to have to include variables defined in "new"
+  		render 'new' 
   		flash[:error]= "There was an error with your reading"
   	end 
+  end
+
+  def edit 
+    @button="Update"
+  end
+
+  def update
+    @button="Update"
+    if @reading.update(reading_params)
+      flash[:success]="Update Successful"
+      redirect_to root_path
+    else 
+      flash[:error]= "Sorry... we could not update your reading"
+      render 'edit'
+    end 
   end
 
   def index
   	@readings=Reading.order("created_at DESC")
   end
 
+  def destroy
+    @reading.destroy
+    redirect_to root_path
+    flash[:notice]="Your reading has been removed"
+  end
+
   private
   	def reading_params
   		params.require(:reading).permit(:sugar_level)
   	end
+
+    def get_reading
+      @reading=Reading.find(params[:id])
+    end
 end
--- app/models/reading.rb
+++ app/models/reading.rb
@@ -2,6 +2,10 @@ class Reading < ActiveRecord::Base
 
 	validates :sugar_level, presence:true, numericality: {greater_than_or_equal_to: 0}
 	
+
+  def self.recent_readings
+    order("created_at desc").limit(5)
+  end
 end
 
   
